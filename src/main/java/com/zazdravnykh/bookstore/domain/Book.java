6b6918a738a8980c54381428885e5ee28ef119eb
Yevgeny Zazdravnykh <yevgeny.zazdravnykh@gmail.com>
2017-01-10 12:29:17 +0200

    persistence unit modified, beans and namespaces added

--- src/main/java/com/zazdravnykh/bookstore/domain/Book.java
+++ src/main/java/com/zazdravnykh/bookstore/domain/Book.java
@@ -1,5 +1,81 @@
 package com.zazdravnykh.bookstore.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "Book")
 public class Book {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	Long id;
+
+	@Column(name = "TITLE")
+	String title;
+
+	@Column(name = "AUTHOR")
+	String author;
+
+	public Book() {
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
+
+	public Book(String title, String author) {
+		super();
+		this.title = title;
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
